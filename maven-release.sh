ce3d7ee1b9d674d00bc27db310033e2f4f77353c
Francis Lalonde <fralalonde@gmail.com>
2017-02-13 11:46:52 -0500

    prepare-with-pom

--- maven-release.sh
+++ maven-release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Deploy maven artifact in current directory to Maven central repository using maven-release-plugin goals
 stty -echo
-printf "Enter GPG passphrase:"
+printf "Enter GPG passphrase: "
 read GPG_PASSPHRASE
 stty echo
 printf "\n"
 
-mvn release:prepare release:perform -B -e -Darguments=-Dgpg.passphrase=$GPG_PASSPHRASE | tee release.log
+mvn release:prepare-with-pom -B -e | tee release-prepare.log
+mvn release:perform -Darguments=-Dgpg.passphrase=$GPG_PASSPHRASE | tee release-perform.log
